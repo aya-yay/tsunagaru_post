b772bdca4f14cae9f3efaa42a6963acfffe557ae
aya-yay <69228983+aya-yay@users.noreply.github.com>
2020-09-17 01:18:49 +0900

    タグ付け２　インクリメンタルサーチ

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -43,6 +43,12 @@ class TweetsController < ApplicationController
     @comments = @tweet.comments.includes(:user)
   end
 
+  def search
+    return nil if params[:input] == ""
+    tag = Tag.where(['name LIKE ?', "%#{params[:input]}%"] )
+    render json:{ keyword: tag }
+  end
+
 
   private
 
