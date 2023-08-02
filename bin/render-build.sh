6b95b57c163be38f5e9260e5a69d5cbbab0b5830
aya-yay <yabuuchi.a.yano@gmail.com>
2023-08-03 02:02:33 +0900

    bin/render-build.sh書き換え
    各ファイルのupdate

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 # exit on error
+#set -o errexit
+
+#bundle install
+#yarn install
+#yarn build 
+
+#bundle exec rake assets:precompile
+
+bin/render-build.sh内
+#!/usr/bin/env bash
+# exit on error
 set -o errexit
 
 bundle install
-yarn install
-yarn build 
-
 bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
 
