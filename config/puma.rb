102fe45a1a02e8bdc7c7bf8511ca4d94030e58a2
aya-yay <yabuuchi.a.yano@gmail.com>
2023-08-03 01:34:02 +0900

    各ファイルのupdate
    各ファイルのupdate

--- config/puma.rb
+++ config/puma.rb
@@ -24,15 +24,23 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # the concurrency of the application would be max `threads` * `workers`.
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
+<<<<<<< Updated upstream
 #
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+=======
+workers ENV.fetch("WEB_CONCURRENCY") { 4 }
+>>>>>>> Stashed changes
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
+<<<<<<< Updated upstream
 #
 # preload_app!
+=======
+preload_app!
+>>>>>>> Stashed changes
 
 # Allow puma to be restarted by `rails restart` command.
-plugin :tmp_restart
+piugin :tmp_restart
