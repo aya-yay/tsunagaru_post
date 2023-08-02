58af2fb74e958312e9505140bbd6736b7eb07df1
aya-yay <yabuuchi.a.yano@gmail.com>
2023-08-03 02:07:44 +0900

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -8,13 +8,13 @@
 
 #bundle exec rake assets:precompile
 
-bin/render-build.sh内
+#bin/render-build.sh
 #!/usr/bin/env bash
 # exit on error
-set -o errexit
+#set -o errexit
 
-bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
+#bundle install
+#bundle exec rake assets:precompile
+#bundle exec rake assets:clean
+#bundle exec rake db:migrate
 
